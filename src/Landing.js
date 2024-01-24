fc5c120e71ba2537a0259ead30e233c5c35bd793
oxodesktop <oxo4work@gmail.com>
2024-01-24 20:26:37 +0800

    reset

--- src/Landing.js
+++ src/Landing.js
@@ -1,5 +1,3 @@
-import cat from "./images/cat-00.png";
-import btn_p_index from "./images/btn_p_index.png";
 import { useEffect, useState } from "react";
 export default function Landing({ progess, setProgess }) {
   const [btnClass, setBtnClass] = useState("btn btn-blank");
@@ -10,9 +8,9 @@ export default function Landing({ progess, setProgess }) {
     }, "850");
   };
 
-  // useEffect(() => {
-  //   progess === "result" && setBtnClass("btn btn-blank");
-  // },[progess])
+  useEffect(() => {
+    progess === "result" && setBtnClass("btn btn-blank");
+  }, [progess]);
 
   return (
     progess === "landing" && (
@@ -21,7 +19,7 @@ export default function Landing({ progess, setProgess }) {
           <div className="subtitle"></div>
           <div className="title"></div>
           <main>
-            <img className="cat" src={cat} alt="貓"></img>
+            <img className="cat" src="images/cat-00.png" alt="貓"></img>
           </main>
           <p className="p">
             你自認是專業貓奴嗎？
@@ -29,7 +27,7 @@ export default function Landing({ progess, setProgess }) {
             貓奴檢定考，等你來挑戰！
           </p>
           <button className={btnClass} onClick={handleStartBtn}>
-            <img src={btn_p_index} alt="開始檢定"></img>
+            <img src="images/btn_p_index.png" alt="開始檢定"></img>
           </button>
         </div>
       </>
