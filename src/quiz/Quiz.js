9c99270c06ac013bed5ce38784d945b949429d71
oxodesktop <oxo4work@gmail.com>
2024-01-31 20:14:45 +0800

    test

--- src/quiz/Quiz.js
+++ src/quiz/Quiz.js
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { barAni } from "../config";
+import { aAni, barAni } from "../config";
 import cat01 from "./images/cat-01.png";
 import cat02 from "./images/cat-02.png";
 import cat03 from "./images/cat-03.png";
@@ -143,7 +143,12 @@ function QuestionItem({ questionList, handleClickAnswer, text, isActive }) {
                 handleClickAnswer(qOption[j].isCorrect, `${text}${j}`)
               }
             >
-              <button className={`answer-${j} p`}>{qOption[j].text}</button>
+              <button
+                className={`answer-${j} p`}
+                style={{ animation: aAni.at(j)[0] }}
+              >
+                {qOption[j].text}
+              </button>
             </li>
           ))}
         </ul>
