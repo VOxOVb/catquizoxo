3d2e156948c99d388d95e26f789b5b384980c2aa
oxo <oxo4work@gmail.com>
2024-02-06 13:55:11 +0800

    fix

--- src/answer/Answer.js
+++ src/answer/Answer.js
@@ -25,44 +25,45 @@ export default function Answer({
         <div className="answer-list">
           {questions.map((item, index) => (
             <div key={`q-${index}`} className="answer-box">
-              <h3>
-                Q.{item.question}
-                <span>
-                <button
-                  key={index}
-                  onClick={() => handleToggle(index)}
-                >
-                  {item.toggle === false ? (
-                            <svg
-                              clip-rule="evenodd"
-                              fill-rule="evenodd"
-                              stroke-linejoin="round"
-                              stroke-miterlimit="2"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path fill="#444" d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z" />
-                            </svg>
-                          ) : (
-                            <svg
-                              clip-rule="evenodd"
-                              fill-rule="evenodd"
-                              stroke-linejoin="round"
-                              stroke-miterlimit="2"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path fill="#444" d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z" />
-                            </svg>
-                          )}
-                </button>
-                </span>
-                {item.score === false ? (
-                  <img src={cross} alt="錯誤標示"></img>
+              <button key={index} onClick={() => handleToggle(index)}>
+                <h3>Q.{item.question}</h3>
+                {item.toggle === false ? (
+                  <svg
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    strokeLinejoin="round"
+                    strokeMiterlimit="2"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill="#444"
+                      d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z"
+                    />
+                  </svg>
                 ) : (
-                  ""
+                  <svg
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    strokeLinejoin="round"
+                    strokeMiterlimit="2"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill="#444"
+                      d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z"
+                    />
+                  </svg>
                 )}
-              </h3>
+                <div className="cross">
+                  {item.score === false ? (
+                    <img src={cross} alt="錯誤標示"></img>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </button>
               {item.toggle === false ? "" : <p className="p">A:{item.parse}</p>}
             </div>
           ))}
--- src/questionList/QuestionList.js
+++ src/questionList/QuestionList.js
@@ -58,39 +58,38 @@ export default function QuestionList({ progress }) {
                 (item, index) =>
                   item.tag === "behavior" && (
                     <div key={`q-${index}`} className="question-box">
-                      <h3>
-                        Q.{item.question}
-                        <button
-                          key={index}
-                          onClick={() => handleToggle(index)}
-                          style={{ fontSize: "25px", color: "#444" }}
-                        >
-                          {item.toggle === false ? (
-                            <svg
-                              clip-rule="evenodd"
-                              fill-rule="evenodd"
-                              stroke-linejoin="round"
-                              stroke-miterlimit="2"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path fill="#444" d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z" />
-                            </svg>
-                          ) : (
-                            <svg
-                              clip-rule="evenodd"
-                              fill-rule="evenodd"
-                              stroke-linejoin="round"
-                              stroke-miterlimit="2"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path fill="#444" d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z" />
-                            </svg>
-                          )}
-                        </button>
-                      </h3>
-
+                      <button key={index} onClick={() => handleToggle(index)}>
+                        <h3>Q.{item.question}</h3>
+                        {item.toggle === false ? (
+                          <svg
+                            clipRule="evenodd"
+                            fillRule="evenodd"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill="#444"
+                              d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z"
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            clipRule="evenodd"
+                            fillRule="evenodd"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill="#444"
+                              d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z"
+                            />
+                          </svg>
+                        )}
+                      </button>
                       {item.toggle === false ? (
                         ""
                       ) : (
@@ -111,39 +110,38 @@ export default function QuestionList({ progress }) {
                 (item, index) =>
                   item.tag === "food" && (
                     <div key={`q-${index}`} className="question-box">
-                      <h3>
-                        Q.{item.question}
-                        <button
-                          key={index}
-                          onClick={() => handleToggle(index)}
-                          style={{ fontSize: "25px", color: "#444" }}
-                        >
-                          {item.toggle === false ? (
-                            <svg
-                              clip-rule="evenodd"
-                              fill-rule="evenodd"
-                              stroke-linejoin="round"
-                              stroke-miterlimit="2"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path fill="#444" d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z" />
-                            </svg>
-                          ) : (
-                            <svg
-                              clip-rule="evenodd"
-                              fill-rule="evenodd"
-                              stroke-linejoin="round"
-                              stroke-miterlimit="2"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path fill="#444" d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z" />
-                            </svg>
-                          )}
-                        </button>
-                      </h3>
-
+                      <button key={index} onClick={() => handleToggle(index)}>
+                        <h3>Q.{item.question}</h3>
+                        {item.toggle === false ? (
+                          <svg
+                            clipRule="evenodd"
+                            fillRule="evenodd"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill="#444"
+                              d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z"
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            clipRule="evenodd"
+                            fillRule="evenodd"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill="#444"
+                              d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z"
+                            />
+                          </svg>
+                        )}
+                      </button>
                       {item.toggle === false ? (
                         ""
                       ) : (
@@ -164,38 +162,38 @@ export default function QuestionList({ progress }) {
                 (item, index) =>
                   item.tag === "item" && (
                     <div key={`q-${index}`} className="question-box">
-                      <h3>
-                        Q.{item.question}
-                        <button
-                          key={index}
-                          onClick={() => handleToggle(index)}
-                        >
-                          {item.toggle === false ? (
-                            <svg
-                              clip-rule="evenodd"
-                              fill-rule="evenodd"
-                              stroke-linejoin="round"
-                              stroke-miterlimit="2"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path fill="#444" d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z" />
-                            </svg>
-                          ) : (
-                            <svg
-                              clip-rule="evenodd"
-                              fill-rule="evenodd"
-                              stroke-linejoin="round"
-                              stroke-miterlimit="2"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path fill="#444" d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z" />
-                            </svg>
-                          )}
-                        </button>
-                      </h3>
-
+                      <button key={index} onClick={() => handleToggle(index)}>
+                        <h3>Q.{item.question}</h3>
+                        {item.toggle === false ? (
+                          <svg
+                            clipRule="evenodd"
+                            fillRule="evenodd"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill="#444"
+                              d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z"
+                            />
+                          </svg>
+                        ) : (
+                          <svg
+                            clipRule="evenodd"
+                            fillRule="evenodd"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="2"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill="#444"
+                              d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z"
+                            />
+                          </svg>
+                        )}
+                      </button>
                       {item.toggle === false ? (
                         ""
                       ) : (
